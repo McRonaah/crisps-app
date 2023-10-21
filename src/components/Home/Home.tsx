ae7d447ab22755e277a105f2317a016eb7f0a2fa
McRonaah <ronaldcheruiyot342@gmail.com>
2023-10-21 10:14:36 +0300

    :sparkles: updated navbar :lipstick:

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,10 @@
+import Navigation from "../Navigation/Navigation";
+import "./Home.css";
+
 function Home() {
     return (
-        <div>
+        <div className="home-container">
+            <Navigation />
             <h1>Hello world</h1>
             <p>This react-firebase programming</p>
         </div>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,33 @@
 import { Link } from 'react-router-dom';
 
+import logo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.png";
+
+import "./Navigation.css";
+
+
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <nav className='navbar'>
+      <div className="logo">
+        <img src={logo} alt="our logo" />
+      </div>
+      <ul className="nav-links">
         <li>
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+      <li>
+        <Link to="/services">Services</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
       </ul>
-    </nav>
+    <div className="avatar">
+      <img src={avatar} alt="Your avatar" />
+    </div>
+  </nav>
+
   );
 };
 
